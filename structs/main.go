68b9957a3742caa68d890a4e637033619a063f8e
sachin supunthaka <sachinsupunthaka96@gmail.com>
2022-11-20 10:19:59 +0530

    update structs

---
+++ structs/main.go
@@ -0,0 +1,16 @@
+package main
+
+// import "fmt"
+
+type person struct {
+	firstName string
+	lastName  string
+}
+
+func main() {
+	// creeate a struct that represent a person
+	// person will have a first name and a last name
+	// create a variable of type person and assign a value to it
+	alex := person{firstName: "Alex", lastName: "Anderson"}
+	fmt.println(alex)
+}
